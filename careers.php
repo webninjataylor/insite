ab1fd9f9701d672421c25c18a062c82fad1df310
Taylor Johnson <webninjataylor@gmail.com>
2013-02-20 14:33:33 -0500

    New careers

--- careers.php
+++ careers.php
@@ -17,7 +17,9 @@
                     <p>We are always looking for motivated professionals that share our vision to excel in our industry. We periodically post our most specific needs here; however, if you are seeking an exciting employment opportunity and you don’t see a position below that fits your expertise, we encourage you to contact us – we’d love to meet with you:</p>  
                     <ul>
                         <li><a href="#projectmanager" class="career">Project Manager</a></li>
-                        <li><a href="#designengineer" class="career">Design Engineer</a></li> 
+                        <li><a href="#designengineer" class="career">Design Engineer</a></li>
+                        <li><a href="#civilengineer" class="career">Civil/Structural Engineer</a></li>
+                        <li><a href="#landscapedesigner" class="career">Landscape Designer</a></li>
                         <li><a href="#draftperson" class="career">Draftperson</a></li>
                     </ul>
                     <p>Our benefits package features:</p>
@@ -35,26 +37,52 @@
                     <p>We encourage fresh ideas and provide you with the tools and resources to see them come to life. We support your professional growth with:</p>
                     <ul>
                         <li>Internal and external training programs</li>
-                        <li>PE continuing education</li>
+                        <li>Pursuit of professional licensure</li>
+                        <li>Professional licensure continuing education</li>
                         <li>Professional license renewals</li>
                     </ul>
                     <h2>Life at InSite</h2>
-                    <p>We encourage employees to think outside the box, innovate, and explore. We understand that it's our employees and their skills that make this company exceptional and we focus to ensure the vitality of our organization and every employee.</p>
+                    <p>We encourage our team to think outside the box, innovate, and explore. We understand that it's our team’s collective skills that make this company exceptional.  We focus to insure the vitality of our organization and promote professional growth of every team member.  We understand a career is not just a 9 to 5 occupation, it is a way of life.  It is our goal to provide each team member an opportunity for a challenging, rewarding, and enjoyable career at InSite.</p>
                 </div>
             </div>
             <div id="careers">
+                
+
                 <a name="projectmanager"></a>
                 <h2>Project Manager:</h2>
                 <p><strong>Responsibilities include but are not limited to:</strong><br/>
                     The qualified candidate must manage and direct assigned staff to meet scope, schedule and budgetary goals for a variety of projects. The candidate must supervise the preparation of reports, site and subdivision plans, specifications, and other contract documents, and review and approve these documents. The candidate must foster the use of innovative concepts in the development of project materials, and prepare more complex project materials. The candidate must assign tasks and monitor staff performance to ensure all schedules and budgets are maintained, and that project goals are met to the satisfaction of the client. The candidate must approve all project schedules, budgets and QA/QC plans. The candidate will be required to attend weekly internal meeting to discuss project progress and to assist in the preparation of proposals, contracts and presentations.</p>
                 <p><strong>Experience required:</strong><br />
                     The successful candidate will possess, at minimum, a bachelor’s degree, and at least five (5) years of experience in Land Development Design, and must have experience with management of site plan and subdivision preparation and stormwater management design.  Candidates should have extensive technical knowledge and the ability to successfully manage both manpower and financial resources. Some field work and attendance at client functions may be required.</p>
+                
+
                 <a name="designengineer"></a>
                 <h2 class="divider">Design Engineer:</h2>
                 <p><strong>Responsibilities:</strong><br />
                     The qualified candidate must assist the Project Manager and/or Professional Engineer with the design of land development projects.  The candidate must gather data from pertinent regulations and available mapping to assist in preparing basemaps for each project.  The candidate must be able to prepare basic design criteria such as grading, construction details, zoning analyses, and must have a working knowledge of stormwater management designs.</p>
                 <p><strong>Experience required:</strong><br />
                     The qualified candidate must have at least two (2) years experience working in the capacity described above.  The candidate must be proficient in AutoCAD version 2000 or greater, as well as Land Desktop software.  A working knowledge of HyrdoCAD and GIS is a plus.  The candidate must have an accredited bachelor’s degree in civil engineering, and must have EIT certification or be on track to receive same.  It is preferable that the candidate seek Professional Engineering licensure.</p>
+
+
+                <a name="civilengineer"></a>
+                <h2 class="divider">Civil/Structural Engineer</h2>
+                <p><strong>Responsibilities:</strong><br />
+                    The qualified candidate must assist the Project Manager and/or Professional Engineer with preparing plan sets for subdivisions and site plans.  The candidate must gather data from pertinent regulations and available mapping to assist in preparing basemaps for each project.  The candidate must be able to prepare basic subdivision and site plan design criteria such as grading, construction details, zoning analyses, earthworks analyses, and must have a working knowledge of stormwater management designs.</p>
+                <p>The qualified candidate must also have the experience necessary to assist the Project Manager and/or Professional Engineer with the design and analysis of various structures including steel platforms, telecommunications towers, retaining walls, bulkheads, timber piles, helical piles, foundation systems, and concrete structures.  The candidate will also be required to perform site investigations and inspections of said structures.</p>
+                <p>The candidate must be proficient in the various engineering software programs (e.g. AutoCAD, TNX Tower (formerly RISA Tower), and Pile Buck).  The candidate must have the ability to prepare written technical reports discussing the items described above.  The candidate must assist the Project Manager and/or Professional Engineer in drafting plan sets for subdivisions, site plans, plot plans, and structural designs.  Drafting work includes, but is not limited to: digital terrain modeling, cross sections, profiles, road templates, earthworks analyses, construction details, key maps, raster images, external references, and layer management.  The candidate will be responsible to ensure consistency of plan appearance prior to submission to the Project Manager and/or Professional Engineer for review.  The candidate must develop a thorough knowledge of the company’s CAD Standards manual.</p>
+                <p><strong>Experience required:</strong><br />
+                The qualified candidate must have at least three (3) to five (5) years’ experience working in the capacity described above. It is preferred that the candidate has an accredited bachelor’s degree in engineering and is on track to become a licensed Professional Engineer.</p>
+
+
+                <a name="landscapedesigner"></a>
+                <h2 class="divider">Landscape Designer</h2>
+                <p><strong>Responsibilities:</strong><br />
+                The qualified candidate must assist the Project Manager and/or Professional Engineer with the design of land development projects.  Key responsibilities are management and preparation of all landscape designs.  The candidate must gather data from pertinent regulations and available mapping to assist in preparing basemaps for each project.  Beyond having a thorough knowledge of landscape and hardscape design, the candidate must be able to prepare basic subdivision and site plan design criteria such as grading, construction details, zoning analyses, earthworks analyses, and must have a working knowledge of stormwater management designs.</p>
+                <p>The candidate must also be proficient in AutoCAD.  The candidate must assist the Project Manager and/or Professional Engineer in preparing plan sets for subdivisions, site plans, plot plans, and structural designs.  Drafting work includes, but is not limited to: digital terrain modeling, cross sections, profiles, road templates, earthworks analyses, construction details, key maps, raster images, external references, and layer management.  The candidate will be responsible to ensure consistency of plan appearance prior to submission to the Project Manager and/or Professional Engineer for review.  The candidate must develop a thorough knowledge of the company’s CAD Standards manual.</p>
+                <p><strong>Experience required:</strong><br />
+                The qualified candidate must have at least three (3) years’ experience working in the capacity described above.  It is preferred that the candidate has an accredited bachelor’s degree in landscape architecture and is on track to become licensed as same.</p>
+
+
                 <a name="draftperson"></a>
                 <h2 class="divider">Draftperson:</h2>
                 <p><strong>Responsibilities:</strong><br/>
