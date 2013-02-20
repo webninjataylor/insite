e4e1d59bb283979017f1c97fd0f88013778576df
Taylor Johnson <webninjataylor@gmail.com>
2013-02-20 03:34:35 -0500

    Rep. Projects

--- projects.php
+++ projects.php
@@ -13,23 +13,65 @@
             <h1>Representative Projects</h1>
             <div class="column left">
                 <ul>
-                    <li><a href="#" data-id="avn" class="peekaboo">AVN</a></li>
-                    <li><a href="#" data-id="avidan" class="peekaboo">Avidan</a></li>
-                    <li><a href="#" data-id="sleven" class="peekaboo">7-Eleven</a></li>
-                    <li><a href="#" data-id="homewood" class="peekaboo">Homewood Suites by Hilton</a></li>
-                    <li><a href="#" data-id="newmark" class="peekaboo">Newmark Schools</a></li>
-                    <li><a href="#" data-id="popeyes" class="peekaboo">Popeyes</a></li>
-                    <li><a href="#" data-id="applebees" class="peekaboo">Applebees</a></li>
-                    <li><a href="#" data-id="sharkriver" class="peekaboo">Shark River Beach and Yacht Club</a></li>
-                    <li><a href="#" data-id="wall" class="peekaboo">Residential Retaining Wall</a></li>
-                    <li><a href="#" data-id="kmb" class="peekaboo">KMB Group</a></li>
-                    <li><a href="#" data-id="boulder" class="peekaboo">Boulder Beach</a></li>
-                    <li><a href="#" data-id="sleep" class="peekaboo">Sleep Center and Medical Office Building</a></li>
-                    <li><a href="#" data-id="spc" class="peekaboo">Shrewsbury Physician's Center</a></li>
-                    <li><a href="#" data-id="brick" class="peekaboo">Brick Recycling</a></li>
+                    <li><a href="javascript:return false;" data-id="brick" class="peekaboo">Brick Recycling</a></li>
+                    <li><a href="javascript:return false;" data-id="dipiero" class="peekaboo">DiPiero Dock</a></li>
+                    <li><a href="javascript:return false;" data-id="efco" class="peekaboo">EFCO</a></li>
+                    <li><a href="javascript:return false;" data-id="mcbw" class="peekaboo">MCBW</a></li>
+                    <li><a href="javascript:return false;" data-id="pb" class="peekaboo">Pep Boys</a></li>
+                    <li><a href="javascript:return false;" data-id="rs" class="peekaboo">Residential Subdivision</a></li>
+                    <li><a href="javascript:return false;" data-id="avn" class="peekaboo">AVN</a></li>
+                    <li><a href="javascript:return false;" data-id="avidan" class="peekaboo">Avidan</a></li>
+                    <li><a href="javascript:return false;" data-id="sleven" class="peekaboo">7-Eleven</a></li>
+                    <li><a href="javascript:return false;" data-id="homewood" class="peekaboo">Homewood Suites by Hilton</a></li>
+                    <li><a href="javascript:return false;" data-id="newmark" class="peekaboo">Newmark Schools</a></li>
+                    <li><a href="javascript:return false;" data-id="popeyes" class="peekaboo">Popeyes</a></li>
+                    <li><a href="javascript:return false;" data-id="applebees" class="peekaboo">Applebees</a></li>
+                    <li><a href="javascript:return false;" data-id="rvc" class="peekaboo">Residential V-Zone Construction</a></li>
+                    <li><a href="javascript:return false;" data-id="rrcrw" class="peekaboo">Residential Reinforced Concrete Retaining Wall</a></li>
+                    <li><a href="javascript:return false;" data-id="rrw" class="peekaboo">Residential Retaining Wall</a></li>
+                    <li><a href="javascript:return false;" data-id="rhefd" class="peekaboo">Residential House Elevation &amp; Foundation Design</a></li>
+                    <li><a href="javascript:return false;" data-id="bayhead" class="peekaboo">Bay Head Yacht Club Waterfront Improvements</a></li>
+                    <li><a href="javascript:return false;" data-id="rcpic" class="peekaboo">Residential &amp; Commercial Pile Inspection &amp; Certification</a></li>
+                    <li><a href="javascript:return false;" data-id="cbrd" class="peekaboo">Commercial Bulkhead Replacement Design</a></li>
+                    <!--
+                        <li><a href="javascript:return false;" data-id="sharkriver" class="peekaboo">Shark River Beach and Yacht Club</a></li>
+                        <li><a href="javascript:return false;" data-id="wall" class="peekaboo">Residential Retaining Wall</a></li>
+                        <li><a href="javascript:return false;" data-id="kmb" class="peekaboo">KMB Group</a></li>
+                        <li><a href="javascript:return false;" data-id="boulder" class="peekaboo">Boulder Beach</a></li>
+                        <li><a href="javascript:return false;" data-id="sleep" class="peekaboo">Sleep Center and Medical Office Building</a></li>
+                        <li><a href="javascript:return false;" data-id="spc" class="peekaboo">Shrewsbury Physician's Center</a></li>
+                    -->
                 </ul>
             </div>
             <div class="column left">
+                <div class="peekaboo" id="brick">
+                    <strong>Brick Recycling</strong><br />
+                    <em>Brick, NJ</em><br />
+                    <img src="images/brick.png" class="photo">
+                </div>
+                <div class="peekaboo" id="dipiero">
+                    <strong>DiPiero Dock</strong><br />
+                    <img src="images/dipiero.png" class="photo">
+                </div>
+                <div class="peekaboo" id="efco">
+                    <strong>EFCO</strong><br />
+                    <img src="images/efco.png" class="photo">
+                </div>
+                <div class="peekaboo" id="mcbw">
+                    <strong>MCBW</strong><br />
+                    <img src="images/mcbw.png" class="photo">
+                </div>
+                <div class="peekaboo" id="pb">
+                    <strong>Pep Boys</strong><br />
+                    <em>Blackwood, NJ</em><br />
+                    <img src="images/pb.png" class="photo">
+                    <img src="images/pb.jpg" class="photo">
+                </div>
+                <div class="peekaboo" id="rs">
+                    <strong>Residential Subdivision</strong><br />
+                    <em>Wall, NJ</em><br />
+                    <img src="images/rs.png" class="photo">
+                </div>
                 <div class="peekaboo" id="avn">
                     <strong>AVN</strong><br />
                     <em>Cranbury, NJ</em>
@@ -65,35 +107,82 @@
                     <em>Tinton Falls, NJ</em>
                     <p>Use variance for outdoor dining and patio bar</p>
                 </div>
-                <div class="peekaboo" id="sharkriver">
-                    <strong>Shark River Beach and Yacht Club</strong><br />
-                    <em>Asbury Park, NJ</em>
+                <div class="peekaboo" id="rvc">
+                    <strong>Residential V-Zone Construction</strong><br />
+                    <em>Monmouth Beach, NJ</em>
+                    <p>Foundation design including timber piles, concrete slab, reinforced concrete masonry piers &amp; foundation-flooring connections; Compliant with FEMA and NJDEP standards</p>
+                    <img src="images/rvc1.jpg" class="photo">
+                    <img src="images/rvc2.jpg" class="photo">
                 </div>
-                <div class="peekaboo" id="wall">
+                <div class="peekaboo" id="rrcrw">
+                    <strong>Residential Reinforced Concrete Retaining Wall</strong><br />
+                    <em>Wall, NJ</em>
+                    <p>Value-engineered reinforced concrete wall design for elevated patio and in-ground swimming pool</p>
+                    <img src="images/rrcrw1.png" class="photo">
+                    <img src="images/rrcrw2.gif" class="photo">
+                </div>
+                <div class="peekaboo" id="rrw">
                     <strong>Residential Retaining Wall</strong><br />
-                    <em>NJ</em>
+                    <em>Holmdel, NJ</em>
+                    <p>Three-tier, segmental block retaining wall design and global stability analysis</p>
+                    <img src="images/rrw1.jpg" class="photo">
+                    <img src="images/rrw2.jpg" class="photo">
                 </div>
-                <div class="peekaboo" id="kmb">
-                    <strong>KMB Group</strong><br />
+                <div class="peekaboo" id="rhefd">
+                    <strong>Residential House Elevation &amp; Foundation Design</strong><br />
+                    <em>Manasquan, NJ</em>
+                    <p>Foundation replacement design (reinforced concrete slab, walls &amp; piers) for residential dwelling located in flood zone</p>
+                    <img src="images/rhefd.jpg" class="photo">
                 </div>
-                <div class="peekaboo" id="boulder">
-                    <strong>Boulder Beach</strong><br />
-                    <em>Long Branch, NJ</em>
-                    <p>Residential subdivision</p>
+                <div class="peekaboo" id="bayhead">
+                    <strong>Bay Head Yacht Club Waterfront Improvements</strong><br />
+                    <em>Bay Head, NJ</em>
+                    <p>Developed comprehensive waterfront improvement plan for Club.  Designed replacement fixed timber docks, breakwaters, bulkheads &amp; concrete slab on site</p>
+                    <img src="images/bayhead1.jpg" class="photo">
+                    <img src="images/bayhead2.jpg" class="photo">
                 </div>
-                <div class="peekaboo" id="sleep">
-                    <strong>Sleep Center and Medical Office Building</strong><br />
-                    <em>Middletown, NJ</em>
-                    <p>15,000 SF professional state of the art building</p>
+                <div class="peekaboo" id="rcpic">
+                    <strong>Residential &amp; Commercial Pile Inspection &amp; Certification</strong><br />
+                    <em>Various Locations</em>
+                    <p>Timber and helical pile installation inspection and certification using industry-standards formulas and calculations.  Pile certification for various types of structures including residential &amp; commercial buildings, waterfront structures &amp; in-ground swimming pools</p>
+                    <img src="images/rcpic1.jpg" class="photo">
+                    <img src="images/rcpic2.jpg" class="photo">
                 </div>
-                <div class="peekaboo" id="spc">
-                    <strong>Shrewsbury Physician's Center</strong><br />
-                    <em>Shrewsbury, NJ</em>
-                    <p>Solar panels</p>
+                <div class="peekaboo" id="cbrd">
+                    <strong>Commercial Bulkhead Replacement Design</strong><br />
+                    <em>Neptune, NJ</em>
+                    <p>Navy-style vinyl bulkhead design to replace failing timber bulkhead</p>
+                    <img src="images/cbrd1.jpg" class="photo">
+                    <img src="images/cbrd2.jpg" class="photo">
                 </div>
-                <div class="peekaboo" id="brick">
-                    <strong>Brick Recycling</strong><br />
-                </div>
+                <!--
+                    <div class="peekaboo" id="sharkriver">
+                        <strong>Shark River Beach and Yacht Club</strong><br />
+                        <em>Asbury Park, NJ</em>
+                    </div>
+                    <div class="peekaboo" id="wall">
+                        <strong>Residential Retaining Wall</strong><br />
+                        <em>NJ</em>
+                    </div>
+                    <div class="peekaboo" id="kmb">
+                        <strong>KMB Group</strong><br />
+                    </div>
+                    <div class="peekaboo" id="boulder">
+                        <strong>Boulder Beach</strong><br />
+                        <em>Long Branch, NJ</em>
+                        <p>Residential subdivision</p>
+                    </div>
+                    <div class="peekaboo" id="sleep">
+                        <strong>Sleep Center and Medical Office Building</strong><br />
+                        <em>Middletown, NJ</em>
+                        <p>15,000 SF professional state of the art building</p>
+                    </div>
+                    <div class="peekaboo" id="spc">
+                        <strong>Shrewsbury Physician's Center</strong><br />
+                        <em>Shrewsbury, NJ</em>
+                        <p>Solar panels</p>
+                    </div>
+                -->
             </div>
         </article>
     </section>
