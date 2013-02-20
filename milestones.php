67216767b88285456d1b4f6133e1e71c187a99c3
Taylor Johnson <webninjataylor@gmail.com>
2013-02-20 02:09:24 -0500

    Edits per documents

--- milestones.php
+++ milestones.php
@@ -12,6 +12,9 @@
         <article>
             <h1>Milestones</h1>
             <ul>
+                <li>2013, February: InSite Engineering celebrates its 10 year anniversary!</li>
+                <li>2013, January: <a href="projects.php?pab=pb">Pep Boys Tire and Service Center</a> [link to representative projects page for this project] of Blackwood, NJ opens it’s doors from a ground-up construction, approved by the Planning Board only 7 months prior</li>
+                <li>2013, January: <a href="projects.php?pab=homewood">Homewood Suites by Hilton</a> [link to representative projects page for this project], a 131-room hotel in Branchburg, NJ receives municipal approval</li>
                 <li>2012, December: &quot;<a href="http://www.erosioncontrol.com/EC/Articles/19111.aspx?page=1" target="_blank">Holding Steady</a>,&quot; Erosion Control Magazine - InSite Engineering, LLC assists to mitigate a structural issue following Hurricane Irene</li>
                 <li>2012, March: InSite Engineering welcomes <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=karakashian">Lory A. Karakashian, PE</a></li>
                 <li>2008, August: InSite Engineering welcomes <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=freud">Robert P. Freud, PE, PP</a></li>
