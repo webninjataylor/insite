e5c97ac94183414a5e5bdb90a057168383c33ff3
Taylor Johnson <webninjataylor@gmail.com>
2013-02-20 16:49:46 -0500

    Updated PP

--- services/professionalplanning.php
+++ services/professionalplanning.php
@@ -12,7 +12,7 @@
         <article>
             <h1>Professional Planning</h1>
             <p>Professional Planners play an essential role in our land development design team.  Each development must be a compliment to its neighborhood and utilize or improve existing infrastructure.  Consistency with the municipality’s Master Plan and Zoning Ordinance as well as the State Plan must be considered.  With proper Planning, land development design projects typically experience not only faster approvals and more public support, but also greater financial success once constructed.  Our licensed Professional Planners provide the following services:</p>
-            <img src="../images/pp.jpg" alt="" class="photo-right">
+            <img src="../images/pp.png" alt="" class="photo-right">
             <ul>
                 <li>Due Diligence Studies</li>
                 <li>Site Planning</li>
