eb9d83c48faf996c09f17db3ac1c3ea9649ff16b
Taylor Johnson <webninjataylor@gmail.com>
2013-02-20 14:52:43 -0500

    Updated milestones

--- milestones.php
+++ milestones.php
@@ -13,13 +13,16 @@
             <h1>Milestones</h1>
             <ul>
                 <li>2013, February: InSite Engineering celebrates its 10 year anniversary!</li>
-                <li>2013, January: <a href="projects.php?pab=pb">Pep Boys Tire and Service Center</a> [link to representative projects page for this project] of Blackwood, NJ opens it’s doors from a ground-up construction, approved by the Planning Board only 7 months prior</li>
-                <li>2013, January: <a href="projects.php?pab=homewood">Homewood Suites by Hilton</a> [link to representative projects page for this project], a 131-room hotel in Branchburg, NJ receives municipal approval</li>
+                <li>2013, January: <a href="projects.php?pab=pb">Pep Boys</a> of Blackwood, NJ opens it’s doors from a ground-up construction, approved by the Planning Board only 7 months prior</li>
+                <li>2013, January: <a href="projects.php?pab=homewood">Homewood Suites by Hilton</a>, a 131-room hotel in Branchburg, NJ receives municipal Site Plan approval</li>
                 <li>2012, December: &quot;<a href="http://www.erosioncontrol.com/EC/Articles/19111.aspx?page=1" target="_blank">Holding Steady</a>,&quot; Erosion Control Magazine - InSite Engineering, LLC assists to mitigate a structural issue following Hurricane Irene</li>
-                <li>2012, March: InSite Engineering welcomes <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=karakashian">Lory A. Karakashian, PE</a></li>
-                <li>2008, August: InSite Engineering welcomes <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=freud">Robert P. Freud, PE, PP</a></li>
-                <li>2005, April: &quot;<a href="http://insiteeng.net/publications/OnsiteInstallerMag_2005_04.pdf" target="_blank">System Profile: Under the Microscope</a>,&quot; OnSite Installer Magazine - InSite Engineering, LLC receives recognition for a challenging septic system design</li>
-                <li>2003, February:   InSite Engineering, LLC was founded by <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=fichter">Jason L. Fichter, PE, PP</a> and <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=ker">Jonathan W. Ker, PE</a></li>
+                <li>2012, March: InSite Engineering welcomes <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=karakashian">Lory A. Karakashian, PE</a> to the team</li>
+                <li>2011, May: <a href="projects.php?pab=newmark">Newmark Schools</a>, a 60,000 SF School for Special Needs Children in Scotch Plains, NJ receives municipal Site Plan approvals.  Doors will open for students in Fall of 2013.</li>
+                <li>2010 November: <a href="projects.php?pab=avn">Hyatt Hotel</a> anchored mixed use development including a 113 room extended stay hotel, a 16,000 sf banquet facility and restaurant and a liquor store receives municipal Use Variance and Site Plan approvals. [link to representative projects page for this project]</li>
+                <li>2008, August: InSite Engineering welcomes <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=freud">Robert P. Freud, PE, PP</a> to the team</li>
+                <li>2007, April: InSite Engineering establishes its residence in Wall Township, NJ</li>
+                <li>2005, April: &quot;<a href="http://insiteeng.net/publications/OnsiteInstallerMag_2005_04.pdf" target="_blank">System Profile: Under the Microscope</a>,&quot; OnSite Installer Magazine - InSite Engineering, LLC receives national recognition for a challenging onsite wastewater system design</li>
+                <li>2003, February: InSite Engineering, LLC was founded by <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=fichter">Jason L. Fichter, PE, PP</a> and <a href="http://www.webninjataylor.com/development/insite/about/keyprofessionals.php?pab=ker">Jonathan W. Ker, PE</a></li>
             </ul>
 
         </article>
