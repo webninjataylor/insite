62b4a91b16a2f834a10a61f605241467912ac085
Taylor Johnson <webninjataylor@gmail.com>
2013-02-20 20:36:04 -0500

    Refactored for GoDaddy and removed old files

--- includes/header.php
+++
@@ -1,5 +0,0 @@
-<header>
-    <a href="/development/insite/index.php"><img src="/development/insite/images/logo-300c.gif" id="logo" alt="InSite Engineering: Engineering, Surveying, and Planning"></a>
-    <a href="services/disasterrecovery.php"><div id="gaudybutton">SUPERSTORM SANDY <br /> Disaster Recovery &amp; Damage Prevention</div></a>
-    <?php include('contact.php'); ?>
-</header>
